a6905e39a7ff6b3dd1cc838d957d5aadbf858182
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2024-08-06 14:34:49 -0700

    test

---
+++ client/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios'
+
+
+function App() {
+  const [data, setData] = useState({})
+
+  const handleClick = (e) => {
+   
+    axios({
+      method: "GET",
+      url: "http://localhost:3000/test"
+    })
+      .then(res => {
+        console.log("res", res)
+        setData(res.data)
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
+  return (
+    <>
+      {console.log("data", data)}
+      <button onClick={(e) => handleClick()}>click</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const fs = require("fs");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const app = express();
+const cors = require("cors");
+
+require("dotenv").config();
+
+app.use(cookieParser());
+
+const port = process.env.PORT || 3000;
+
+app.use(
+    fileUpload({
+        createParentPath: true,
+    })
+);
+app.use("/public/", express.static(process.cwd() + "/public"));
+app.use(
+    cors({
+        methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+        // origin: [`89.116.191.98:5173`],
+        "Access-Control-Allow-Origin": "*",
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
+app.use(express.json());
+
+// const Router = require("./routes/routes");
+// Router(app);
+
+app.get("/test", (req, res) => {
+    // console.log("res", res)
+    res.json({ msg: "got it hommie" })
+})
+
+app.listen(port, () => {
+    //   mongoose.connect(process.env.MONGO_URI).then(() => {
+    //     console.log("connected to Database");
+    //   });
+
+    console.log(`Server is running on port: ${port}`);
+});
+
+
+
