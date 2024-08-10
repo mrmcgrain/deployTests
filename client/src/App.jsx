ff9c4704c970d0ee1c9b8920ce0dc6261846fbea
Michael McGrain <153415900+mrmcgrain@users.noreply.github.com>
2024-08-09 17:29:56 -0700

    img test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,9 +33,11 @@ function App() {
       {/* {console.log("data", data)} */}
 
       {/* <p>{data.msg}</p> */}
-      
+
       {/* <button onClick={(e) => handleClick()}>click</button> */}
+      
       <p>test ci cd</p>
+
 
 
     </>
